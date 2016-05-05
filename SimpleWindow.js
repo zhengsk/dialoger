2801945ffc0051394c161aa862b01d04ab7ab8f6
城管大队长 <zsk526@qq.com>
2016-05-05 16:10:09 +0800

    update

--- SimpleWindow.js
+++ SimpleWindow.js
@@ -7,6 +7,8 @@ SimpleWindow.defaultOptions = {
 	isCollapse : false,	// 是否缩起
 	isMaximize : false, // 是否最大化
 
+	zIndex : 1000,
+
 	parent : document.body,
 
 	width: 300,
@@ -41,11 +43,17 @@ SimpleWindow.prototype = {
 
 	// 初始化
 	init : function() {
-		this._create();
+		var opts = this.options;
+
+		this._create(); // 创建窗口元素
+
+		this.moveTo(opts.left, opts.top);
+		this.resizeTo(opts.width, opts.height);
 	},
 
 	// 创建窗口
 	_create : function () {
+		var opts = this.options;
 
 		// simple-window
 		var winElement = this.winElement = document.createElement('div');
@@ -59,35 +67,39 @@ SimpleWindow.prototype = {
 		// simple-window-header
 		var winHeader = this.winHeader = document.createElement('div');
 		winHeader.className = "simple-window-header";
+		winHeader.innerHTML = opts.header;
 		winContainer.appendChild(winHeader);
 
 		// simple-window-body
 		var winBody = this.winBody = document.createElement('div');
 		winBody.className = "simple-window-body";
+		winBody.innerHTML = opts.body;
 		winContainer.appendChild(winBody);
 
 		// simple-window-footer
 		var winFooter = this.winFooter = document.createElement('div');
 		winFooter.className = "simple-window-footer";
+		winFooter.innerHTML = opts.footer;
 		winContainer.appendChild(winFooter);
 
-		this.parent.appendChild(winElement);
+		this.options.parent.appendChild(winElement);
 	},
 
-	// 销毁窗口
-	destory : function() {
-		
-	},
+	
 
 
 	// 移动到指定位置
 	moveTo : function(left, top) {
-		
+		var style = this.winElement.style;
+		left !== null && (style.left = left + "px");
+		top !== null && (style.top = top + "px");
 	},
 
 	// 改变大小
 	resizeTo : function(width, height) {
-		
+		var style = this.winElement.style;
+		width !== null && (style.width = width + "px");
+		height !== null && (style.height = height + "px");
 	},
 
 
@@ -95,20 +107,25 @@ SimpleWindow.prototype = {
 
 	// 显示
 	show : function() {
-		
+		this.winElement.style.display = "block";
+		this.isVisible = false;
 	},
 
 	hide : function() {
-		
+		this.winElement.style.display = "none";
+		this.isVisible = true;
 	},
 
 	// 切换显示状态
 	toggle : function() {
 		this[this.isVisible ? "hide" : "show"]()
+	},
+
+
+	// 销毁窗口
+	destory : function() {
+		this.winElement.parentNode.removeChild(this.winElement);
 	}
-
-
-
 
 }
 
