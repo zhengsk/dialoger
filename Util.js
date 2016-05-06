4a909b5545c08c4fb06cecba8f955644338f206a
城管大队长 <zsk526@qq.com>
2016-05-06 10:17:42 +0800

    fixed ie8 bug

--- Util.js
+++ Util.js
@@ -6,6 +6,7 @@ var Util = {
 			if (element.addEventListener) {
 				element.addEventListener(type, handler, false);
 			} else if (element.attachEvent) {
+				element.attachEvent("on" + type, handler);
 			} else {
 				element['on' + type] = handler;
 			}
